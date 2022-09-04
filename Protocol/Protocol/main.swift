0ac1da3070acdaa59042a5eb892d957d2a91e342
Mikhail Belov <55504815+MikkiWhiteDove@users.noreply.github.com>
2022-09-05 02:21:29 +0300

    add ProtocolFile

---
+++ Protocol/Protocol/main.swift
@@ -0,0 +1,71 @@
+//
+//  main.swift
+//  Protocol
+//
+//  Created by Mishana on 05.09.2022.
+//
+
+import Foundation
+
+protocol MobileStorage {
+func getAll() -> Set<Mobile>
+func findByImei(_ imei: String) -> Mobile?
+func save(_ mobile: Mobile) throws -> Mobile
+func delete(_ product: Mobile) throws
+func exists(_ product: Mobile) -> Bool
+}
+
+struct Mobile: Hashable {
+let imei: String
+let model: String
+}
+
+class Phone: MobileStorage {
+    
+    var mobileStorage = Set<Mobile>()
+    
+    func getAll() -> Set<Mobile> {
+        return Set(fetchData())
+    }
+    
+    func findByImei(_ imei: String) -> Mobile? {
+        for mobile in mobileStorage{
+            if mobile.imei.lowercased().contains(imei.lowercased()){
+                return mobile
+            }
+        }
+        return nil
+    }
+    
+    func save(_ mobile: Mobile) throws -> Mobile {
+        mobileStorage.insert(mobile)
+        return mobile
+    }
+    
+    func delete(_ product: Mobile) throws {
+        guard let delete = findByImei(product.imei) else {return}
+        mobileStorage.remove(delete)
+    }
+    
+    func exists(_ product: Mobile) -> Bool {
+        if findByImei(product.imei) != nil {
+            return true
+        }else {
+            return false
+        }
+    }
+    
+    
+}
+
+extension Phone {
+    func fetchData() -> [Mobile] {
+        let image1 = Mobile(imei: "123 123 123 132", model: "iPhone12")
+        
+        return [image1, image1, image1]
+    }
+}
+
+
+//Phone.save()
+
